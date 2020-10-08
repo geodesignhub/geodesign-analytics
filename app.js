364fa590ebba094603bd1e4ed1557008d92b7056
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2020-10-08 15:17:00 +0100

    Updated JS and Bootstrap versions + changed path and post requests

--- app.js
+++ app.js
@@ -14,116 +14,120 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json())
 
-app.get('/', function(request, response) {
-    var opts = {};
+
+app.get('/', function (request, response) {
+
     if (request.query.apitoken && request.query.projectid) {
-        opts = { 'apitoken': request.query.apitoken, 'projectid': request.query.projectid };
-    } else {
-        opts = { 'apitoken': '0', 'projectid': '0' };
-    }
+        
+        var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
+        // var baseurl = 'http://local.test:8000/api/v1/projects/';
+        var apikey = request.query.apitoken;
+        var projectid = request.query.projectid;
+        var cred = "Token " + apikey;
+        
+        
+        var systems = baseurl + projectid + '/systems/';
+        var diagrams = baseurl + projectid + '/diagrams/';
+        var cteams = baseurl + projectid + '/cteams/';
+        var members = baseurl + projectid + '/members/';
 
-    response.render('gdhdna', opts);
-});
-
-app.post('/post/', function(request, response) {
-    var baseurl = 'https://www.geodesignhub.com/api/v1/projects/';
-    // var baseurl = 'http://local.test:8000/api/v1/projects/';
-    var apikey = request.body.apikey;
-    var projectid = request.body.projectid;
-    var cred = "Token " + apikey;
-    var systems = baseurl + projectid + '/systems/';
-    var diagrams = baseurl + projectid + '/diagrams/';
-    var cteams = baseurl + projectid + '/cteams/';
-    var members = baseurl + projectid + '/members/';
-
-    var allsyns = [];
-    var URLS = [systems, diagrams, cteams, members];
-    async.map(URLS, function(url, done) {
-        req({
-            url: url,
-            headers: {
-                "Authorization": cred,
-                "Content-Type": "application/json"
-            }
-        }, function(err, response, body) {
-            if (err || response.statusCode !== 200) {
-                return done(err || new Error());
-            }
-            return done(null, JSON.parse(body));
-        });
-    }, function(err, results) {
-        if (err) return response.sendStatus(500);
-        // get the cteams
-        systems = results[0];
-        diagrams = results[1];
-        cteams = results[2];
-        members = results[3];
-        var ctlen = cteams.length;
-        var ctURLS = [];
-        for (var x = 0; x < ctlen; x++) {
-            var ctid = cteams[x]['id'];
-            var cturl = baseurl + projectid + '/cteams/' + ctid + '/';
-            ctURLS.push(cturl);
-        }
-        async.map(ctURLS, function(url, done) {
+        var allsyns = [];
+        var URLS = [systems, diagrams, cteams, members];
+        
+        async.map(URLS, function (url, done) {
             req({
                 url: url,
                 headers: {
                     "Authorization": cred,
                     "Content-Type": "application/json"
                 }
-            }, function(err, response, body) {
+            }, function (err, response, body) {
                 if (err || response.statusCode !== 200) {
                     return done(err || new Error());
                 }
                 return done(null, JSON.parse(body));
             });
-        }, function(err, results) {
+        }, function (err, results) {
             if (err) return response.sendStatus(500);
-            var reslen = results.length;
-            var synURLs = [];
-            for (var k = 0; k < reslen; k++) {
-                var synList = results[k]['synthesis'];
-                var synlength = synList.length;
-                for (var y = 0; y < synlength; y++) {
-                    var cursyn = synList[y];
-                    allsyns.push(cursyn);
-                    var cteamid = cursyn['cteamid'];
-                    var synID = cursyn['id']
-                    var cturl = baseurl + projectid + '/cteams/' + cteamid + '/' + synID + '/diagrams/';
-                    synURLs.push(cturl);
-                }
+            // get the cteams
+            systems = results[0];
+            diagrams = results[1];
+            cteams = results[2];
+            members = results[3];
+            var ctlen = cteams.length;
+            var ctURLS = [];
+            for (var x = 0; x < ctlen; x++) {
+                var ctid = cteams[x]['id'];
+                var cturl = baseurl + projectid + '/cteams/' + ctid + '/';
+                ctURLS.push(cturl);
             }
-
-            async.map(synURLs, function(url, done) {
+            async.map(ctURLS, function (url, done) {
                 req({
                     url: url,
                     headers: {
                         "Authorization": cred,
                         "Content-Type": "application/json"
                     }
-                }, function(err, response, body) {
+                }, function (err, response, body) {
                     if (err || response.statusCode !== 200) {
                         return done(err || new Error());
                     }
                     return done(null, JSON.parse(body));
                 });
-            }, function(err, results) {
+            }, function (err, results) {
                 if (err) return response.sendStatus(500);
-                response.contentType('application/json');
+                var reslen = results.length;
+                var synURLs = [];
+                for (var k = 0; k < reslen; k++) {
+                    var synList = results[k]['synthesis'];
+                    var synlength = synList.length;
+                    for (var y = 0; y < synlength; y++) {
+                        var cursyn = synList[y];
+                        allsyns.push(cursyn);
+                        var cteamid = cursyn['cteamid'];
+                        var synID = cursyn['id']
+                        var cturl = baseurl + projectid + '/cteams/' + cteamid + '/' + synID + '/diagrams/';
+                        synURLs.push(cturl);
+                    }
+                }
 
-                response.send({
-                    "status": 1,
-                    "syndiagrams": results,
-                    "diagrams": diagrams,
-                    "systems": systems,
-                    "cteams": cteams,
-                    "syns": allsyns,
-                    "members": members
+                async.map(synURLs, function (url, done) {
+                    req({
+                        url: url,
+                        headers: {
+                            "Authorization": cred,
+                            "Content-Type": "application/json"
+                        }
+                    }, function (err, response, body) {
+                        if (err || response.statusCode !== 200) {
+                            return done(err || new Error());
+                        }
+                        return done(null, JSON.parse(body));
+                    });
+                }, function (err, results) {
+                    if (err) return response.sendStatus(500);
+                    let opts = {
+                        "apitoken": request.query.apitoken, 
+                        "projectid": request.query.projectid,
+                        "projectdata":{
+                        "status": 1,
+                        "syndiagrams": results,
+                        "diagrams": diagrams,
+                        "systems": systems,
+                        "cteams": cteams,
+                        "syns": allsyns,
+                        "members": members}
+                    }
+                    response.render('gdhdna', opts);
                 });
             });
         });
-    });
+
+    }
+    else {
+        response.status(400).send('Not all query parameters provided.');
+    }
+
 });
 
 app.listen(process.env.PORT || 5001);
